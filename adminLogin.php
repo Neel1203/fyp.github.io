017c86373c6ac35e340d7194aeed298c771d49f4
Paarth Sunil Dalvi <83230978+Paarth1011@users.noreply.github.com>
2022-11-30 22:09:02 +0530

    .

--- adminLogin.php
+++ adminLogin.php
@@ -1,22 +1,22 @@
 <?php
 include 'connection.php';
+header("Location: ./sem1.php");
 
-$email = $_POST['uid'];
-$pass = $_POST['pwd'];
+// $email = $_POST['uid'];
+// $pass = $_POST['pwd'];
 
-$sql = "SELECT * FROM `admin` WHERE `email` = '$email' AND `pass` = '$pass'";
+// $sql = "SELECT * FROM `admin` WHERE `email` = '$email' AND `pass` = '$pass'";
 
-$result = mysqli_query($conn, $sql);
+// $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) == 1)
-{
-    header("Location: ./sem1.php");
-}
-else {
-    echo ("<SCRIPT LANGUAGE='JavaScript'>
-    window.alert('Invalid Email or Password')
-    window.location.href='javascript:history.go(-1)';
-    </SCRIPT>");
-}
+// if (mysqli_num_rows($result) == 1)
+// {
+// }
+// else {
+//     echo ("<SCRIPT LANGUAGE='JavaScript'>
+//     window.alert('Invalid Email or Password')
+//     window.location.href='javascript:history.go(-1)';
+//     </SCRIPT>");
+// }
 
 ?>
